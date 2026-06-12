941df6393f197fd72ce7b39f92fa140909232d28
Kazuya Takahashi <electrolysis.jp@gmail.com>
2026-06-13 01:08:15 +0900

    define Ok, Err, Result

--- source/result.d
+++ source/result.d
@@ -1,8 +1,71 @@
 module result;
 
-import std.stdio;
-
-void main()
+///
+struct Ok(T)
 {
-	writeln("Edit source/app.d to start your project.");
+    ///
+    T okValue;
+    alias okValue this;
+}
+
+///
+@safe @nogc nothrow unittest
+{
+    assert(Ok!int(314).okValue == 314);
+    assert(Ok!int(314) == 314); // alias this
+    assert(Ok!string("Good day.") == "Good day."); // alias this
+}
+
+///
+struct Err(E)
+{
+    ///
+    E errValue;
+    alias errValue this;
+}
+
+///
+@safe @nogc nothrow unittest
+{
+    assert(Err!int(314).errValue == 314);
+    assert(Err!int(314) == 314); // alias this
+    assert(Err!string("Good day.") == "Good day."); // alias this
+}
+
+/// Performs as a `std.sumtype.SumType` but has some additional features.
+struct Result(T, E)
+{
+    import std.sumtype : SumType;
+
+    ///
+    alias Result = SumType!(Ok!T, Err!E);
+    Result result;
+    alias result this;
+
+    /// The constructor accepts `Result!(T, E)`, `Ok!T` and `Err!E` values.
+    this(R)(auto ref R value)
+            if (is(typeof({ Result.init = R.init; })))
+    {
+        this.result = value;
+    }
+
+}
+
+// Ctor
+@safe @nogc nothrow unittest
+{
+    import std.sumtype : has, get;
+
+    auto resultOk = Result!(int, string)(Ok!int(123));
+    assert(resultOk.has!(Ok!int));
+    assert(!resultOk.has!(Err!string));
+    assert(resultOk.get!(Ok!int) == Ok!int(123));
+
+    Result!(bool, dstring) resultErr = Err!dstring("123"d);
+    assert(resultErr.has!(Err!dstring));
+    assert(!resultErr.has!(Ok!bool));
+    assert(resultErr.get!(Err!dstring) == Err!dstring("123"d));
+
+    auto newResult = Result!(bool, dstring)(resultErr);
+    assert(newResult.get!(Err!dstring) == Err!dstring("123"d));
 }
